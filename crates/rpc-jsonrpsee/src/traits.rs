734946e74831992f7e697bea5a7e4f069632810f
refcell <abigger87@gmail.com>
2024-08-25 11:16:16 -0400

    fix(rpc-jsonrpsee): crate rename

--- crates/rpc-jsonrpsee/src/traits.rs
+++ crates/rpc-jsonrpsee/src/traits.rs
@@ -3,8 +3,10 @@
 use jsonrpsee::{RpcResult, proc_macros::rpc};
 use alloy_eips::BlockNumberOrTag;
 use op_alloy_rpc_types::sync::SyncStatus;
+use op_alloy_rpc_types::config::{RollupConfig, OutputResponse};
 
 /// Optimism specified rpc interface.
+///
 /// https://docs.optimism.io/builders/node-operators/json-rpc
 /// https://github.com/ethereum-optimism/optimism/blob/8dd17a7b114a7c25505cd2e15ce4e3d0f7e3f7c1/op-node/node/api.go#L114
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "optimism"))]
@@ -12,20 +14,20 @@ use op_alloy_rpc_types::sync::SyncStatus;
 pub trait RollupNode {
     /// Get the output root at a specific block.
     #[method(name = "outputAtBlock")]
-    async fn optimism_output_at_block(
+    async fn op_output_at_block(
         &self,
         block_number: BlockNumberOrTag,
     ) -> RpcResult<OutputResponse>;
 
     /// Get the synchronization status.
     #[method(name = "syncStatus")]
-    async fn optimism_sync_status(&self) -> RpcResult<SyncStatus>;
+    async fn op_sync_status(&self) -> RpcResult<SyncStatus>;
 
     /// Get the rollup configuration parameters.
     #[method(name = "rollupConfig")]
-    async fn optimism_rollup_config(&self) -> RpcResult<RollupConfig>;
+    async fn op_rollup_config(&self) -> RpcResult<RollupConfig>;
 
     /// Get the software version.
     #[method(name = "version")]
-    async fn optimism_version(&self) -> RpcResult<String>;
+    async fn op_version(&self) -> RpcResult<String>;
 }
