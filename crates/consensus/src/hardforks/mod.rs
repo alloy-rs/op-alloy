ff884289cfbdbaa1da42aabc40e097e2051a6977
refcell <abigger87@gmail.com>
2024-08-29 08:21:08 -0400

    feat: touch up doc comments for hardforks

--- crates/consensus/src/hardforks/mod.rs
+++ crates/consensus/src/hardforks/mod.rs
@@ -11,6 +11,22 @@ pub(crate) const UPGRADE_TO_FUNC_BYTES_4: &[u8] = &[0x36, 0x59, 0xcf, 0xe6];
 /// This type is used to encapsulate hardfork transactions.
 /// It exposes methods that return hardfork upgrade transactions
 /// as [alloy_primitives::Bytes].
+///
+/// # Example
+///
+/// Build ecotone hardfork upgrade transaction:
+/// ```rust
+/// use alloy_consensus::Hardforks;
+/// let ecotone_upgrade_tx = Hardforks::ecotone_txs();
+/// assert_eq!(ecotone_upgrade_tx.len(), 6);
+/// ```
+///
+/// Build fjord hardfork upgrade transactions:
+/// ```rust
+/// use alloy_consensus::Hardforks;
+/// let fjord_upgrade_txs = Hardforks::fjord_txs();
+/// assert_eq!(fjord_upgrade_txs.len(), 3);
+/// ```
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Hardforks;
 
