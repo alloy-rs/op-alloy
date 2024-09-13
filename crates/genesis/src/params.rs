1d53a8a57e559856285789356bfd69445510f017
refcell <abigger87@gmail.com>
2024-09-13 10:11:12 -0400

    fix: re-arrange params

--- crates/genesis/src/params.rs
+++ crates/genesis/src/params.rs
@@ -2,28 +2,6 @@
 
 use alloy_eips::eip1559::BaseFeeParams;
 
-/// Returns the [BaseFeeParams] for the given chain id.
-pub const fn base_fee_params(chain_id: u64) -> BaseFeeParams {
-    match chain_id {
-        10 => OP_BASE_FEE_PARAMS,
-        11155420 => OP_SEPOLIA_BASE_FEE_PARAMS,
-        8453 => OP_BASE_FEE_PARAMS,
-        84532 => BASE_SEPOLIA_BASE_FEE_PARAMS,
-        _ => OP_BASE_FEE_PARAMS,
-    }
-}
-
-/// Returns the Canyon [BaseFeeParams] for the given chain id.
-pub const fn canyon_base_fee_params(chain_id: u64) -> BaseFeeParams {
-    match chain_id {
-        10 => OP_CANYON_BASE_FEE_PARAMS,
-        11155420 => OP_SEPOLIA_CANYON_BASE_FEE_PARAMS,
-        8453 => OP_CANYON_BASE_FEE_PARAMS,
-        84532 => BASE_SEPOLIA_CANYON_BASE_FEE_PARAMS,
-        _ => OP_CANYON_BASE_FEE_PARAMS,
-    }
-}
-
 /// Base fee max change denominator for Optimism Mainnet as defined in the Optimism
 /// [transaction costs](https://community.optimism.io/docs/developers/build/differences/#transaction-costs) doc.
 pub const OP_MAINNET_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR: u128 = 50;
@@ -87,3 +65,25 @@ pub const OP_CANYON_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
     max_change_denominator: OP_MAINNET_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
     elasticity_multiplier: OP_MAINNET_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
 };
+
+/// Returns the [BaseFeeParams] for the given chain id.
+pub const fn base_fee_params(chain_id: u64) -> BaseFeeParams {
+    match chain_id {
+        10 => OP_BASE_FEE_PARAMS,
+        11155420 => OP_SEPOLIA_BASE_FEE_PARAMS,
+        8453 => OP_BASE_FEE_PARAMS,
+        84532 => BASE_SEPOLIA_BASE_FEE_PARAMS,
+        _ => OP_BASE_FEE_PARAMS,
+    }
+}
+
+/// Returns the Canyon [BaseFeeParams] for the given chain id.
+pub const fn canyon_base_fee_params(chain_id: u64) -> BaseFeeParams {
+    match chain_id {
+        10 => OP_CANYON_BASE_FEE_PARAMS,
+        11155420 => OP_SEPOLIA_CANYON_BASE_FEE_PARAMS,
+        8453 => OP_CANYON_BASE_FEE_PARAMS,
+        84532 => BASE_SEPOLIA_CANYON_BASE_FEE_PARAMS,
+        _ => OP_CANYON_BASE_FEE_PARAMS,
+    }
+}
