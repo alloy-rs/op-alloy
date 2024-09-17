94ad5acf4543d1114ab94da4b8a2d1ed2fe66d9c
refcell <abigger87@gmail.com>
2024-09-17 15:00:36 -0400

    fix: errors and derive_more displays

--- crates/rpc-types-engine/src/errors.rs
+++ crates/rpc-types-engine/src/errors.rs
@@ -7,97 +7,55 @@ use op_alloy_protocol::block_info::DecodeError;
 
 /// An error that can occur when converting an [crate::OptimismExecutionPayloadEnvelopeV4] to an
 /// [op_alloy_protocol::L2BlockInfo].
-#[derive(Debug)]
+#[derive(Debug, derive_more::Display)]
 pub enum ToL2BlockRefError {
     /// The genesis block hash does not match the expected value.
+    #[display("Invalid genesis hash")]
     InvalidGenesisHash,
     /// The L2 block is missing the L1 info deposit transaction.
+    #[display("L2 block is missing L1 info deposit transaction ({_0})")]
     MissingL1InfoDeposit(B256),
     /// The first payload transaction has an unexpected type.
+    #[display("First payload transaction has unexpected type: {_0}")]
     UnexpectedTxType(u8),
     /// Failed to decode the first transaction into an [op_alloy_consensus::OpTxEnvelope].
+    #[display("Failed to decode the first transaction into an OpTxEnvelope: {_0}")]
     TxEnvelopeDecodeError(Eip2718Error),
     /// The first payload transaction is not a deposit transaction.
+    #[display("First payload transaction is not a deposit transaction, type: {_0}")]
     FirstTxNonDeposit(u8),
     /// Failed to decode the [op_alloy_protocol::L1BlockInfoTx] from the deposit transaction.
+    #[display("Failed to decode the L1BlockInfoTx from the deposit transaction: {_0}")]
     BlockInfoDecodeError(DecodeError),
 }
 
-#[cfg(feature = "std")]
-impl std::error::Error for ToL2BlockRefError {}
-
-impl core::fmt::Display for ToL2BlockRefError {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            ToL2BlockRefError::InvalidGenesisHash => write!(f, "Invalid genesis hash"),
-            ToL2BlockRefError::MissingL1InfoDeposit(block_hash) => {
-                write!(f, "L2 block is missing L1 info deposit transaction ({})", block_hash)
-            }
-            ToL2BlockRefError::UnexpectedTxType(ty) => {
-                write!(f, "First payload transaction has unexpected type: {}", ty)
-            }
-            ToL2BlockRefError::TxEnvelopeDecodeError(t) => {
-                write!(f, "Failed to decode the first transaction into an OpTxEnvelope: {:?}", t)
-            }
-            ToL2BlockRefError::FirstTxNonDeposit(ty) => {
-                write!(f, "First payload transaction is not a deposit transaction, type: {}", ty)
-            }
-            ToL2BlockRefError::BlockInfoDecodeError(t) => write!(
-                f,
-                "Failed to decode the L1BlockInfoTx from the deposit transaction: {:?}",
-                t
-            ),
-        }
-    }
-}
+impl core::error::Error for ToL2BlockRefError {}
 
 /// An error that can occur when converting an [crate::OptimismExecutionPayloadEnvelopeV4] to a
 /// [op_alloy_genesis::SystemConfig].
-#[derive(Debug)]
+#[derive(Debug, derive_more::Display)]
 pub enum ToSystemConfigError {
     /// The genesis block hash does not match the expected value.
+    #[display("Invalid genesis hash")]
     InvalidGenesisHash,
     /// The L2 block is missing the L1 info deposit transaction.
+    #[display("L2 block is missing L1 info deposit transaction ({_0})")]
     MissingL1InfoDeposit(B256),
     /// The first payload transaction has an unexpected type.
+    #[display("First payload transaction has unexpected type: {_0}")]
     UnexpectedTxType(u8),
     /// Failed to decode the first transaction into an [op_alloy_consensus::OpTxEnvelope].
+    #[display("Failed to decode the first transaction into an OpTxEnvelope: {_0}")]
     TxEnvelopeDecodeError(Eip2718Error),
     /// The first payload transaction is not a deposit transaction.
+    #[display("First payload transaction is not a deposit transaction, type: {_0}")]
     FirstTxNonDeposit(u8),
     /// Failed to decode the [op_alloy_protocol::L1BlockInfoTx] from the deposit transaction.
+    #[display("Failed to decode the L1BlockInfoTx from the deposit transaction: {_0}")]
     BlockInfoDecodeError(DecodeError),
     /// Missing system config in the genesis block.
+    #[display("Missing system config in the genesis block")]
     MissingSystemConfig,
 }
 
-#[cfg(feature = "std")]
-impl std::error::Error for ToSystemConfigError {}
-
-impl core::fmt::Display for ToSystemConfigError {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            ToSystemConfigError::InvalidGenesisHash => write!(f, "Invalid genesis hash"),
-            ToSystemConfigError::MissingL1InfoDeposit(block_hash) => {
-                write!(f, "L2 block is missing L1 info deposit transaction ({})", block_hash)
-            }
-            ToSystemConfigError::UnexpectedTxType(ty) => {
-                write!(f, "First payload transaction has unexpected type: {}", ty)
-            }
-            ToSystemConfigError::TxEnvelopeDecodeError(t) => {
-                write!(f, "Failed to decode the first transaction into an OpTxEnvelope: {:?}", t)
-            }
-            ToSystemConfigError::FirstTxNonDeposit(ty) => {
-                write!(f, "First payload transaction is not a deposit transaction, type: {}", ty)
-            }
-            ToSystemConfigError::BlockInfoDecodeError(t) => write!(
-                f,
-                "Failed to decode the L1BlockInfoTx from the deposit transaction: {:?}",
-                t
-            ),
-            ToSystemConfigError::MissingSystemConfig => {
-                write!(f, "Missing system config in the genesis block")
-            }
-        }
-    }
-}
+impl core::error::Error for ToSystemConfigError {}
