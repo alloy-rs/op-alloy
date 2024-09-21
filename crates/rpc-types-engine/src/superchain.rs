e1f2dd21f91b34acabd0dba41199ec380e082155
nicolas <48695862+merklefruit@users.noreply.github.com>
2024-09-21 18:12:30 +0200

    chore: cleanup

--- crates/rpc-types-engine/src/superchain.rs
+++ crates/rpc-types-engine/src/superchain.rs
@@ -1,7 +1,6 @@
 use core::array::TryFromSliceError;
 
 use alloy_primitives::{B256, B64};
-use cfg_if::cfg_if;
 
 /// Superchain Signal information.
 ///
@@ -37,55 +36,74 @@ pub struct SuperchainSignal {
 ///
 /// See also: <https://specs.optimism.io/protocol/superchain-upgrades.html#protocol-version>
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum ProtocolVersion {
     /// Version-type 0.
     V0(ProtocolVersionFormatV0),
 }
 
-cfg_if! {
-    if #[cfg(feature = "serde")] {
-        use serde::{de, Serialize, Serializer, Deserialize, Deserializer};
+#[derive(Copy, Clone, Debug, thiserror::Error)]
+pub enum ProtocolVersionError {
+    #[error("Unsupported version: {0}")]
+    UnsupportedVersion(u8),
+    #[error("Invalid version format length. Got {0}, expected 31")]
+    InvalidLength(usize),
+    #[error("Invalid version format encoding")]
+    FromSlice(#[from] TryFromSliceError),
+}
 
-        impl Serialize for ProtocolVersion {
-            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-            where
-                S: Serializer,
-            {
-                match self {
-                    Self::V0(value) => {
-                        // <protocol version> ::= <version-type><typed-payload>
-                        // <version-type> ::= <uint8>
-                        // <typed-payload> ::= <31 bytes>
-                        let mut bytes = [0u8; 32];
-                        bytes[0] = 0x00; // this is not necessary, but addded for clarity
-                        bytes[1..].copy_from_slice(&Into::<[u8; 31]>::into(*value));
+impl From<ProtocolVersion> for B256 {
+    fn from(value: ProtocolVersion) -> B256 {
+        let mut bytes = [0u8; 32];
 
-                        B256::from_slice(&bytes).serialize(serializer)
-                    }
-                }
-            }
-        }
-
-        impl<'de> Deserialize<'de> for ProtocolVersion {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-            where
-                D: Deserializer<'de>,
-            {
-                // The version must be encoded as 32 bytes of DATA in JSON RPC usage.
-                let value = B256::deserialize(deserializer)?;
-
+        match value {
+            ProtocolVersion::V0(value) => {
                 // <protocol version> ::= <version-type><typed-payload>
                 // <version-type> ::= <uint8>
                 // <typed-payload> ::= <31 bytes>
-                let version_type = value[0];
-                let typed_payload = &value[1..];
-
-                match version_type {
-                    0 => Ok(Self::V0(ProtocolVersionFormatV0::try_from(typed_payload).map_err(de::Error::custom)?)),
-                    other => Err(de::Error::custom(format!("unsupported protocol version: {}", other))),
-                }
+                bytes[0] = 0x00; // this is not necessary, but addded for clarity
+                bytes[1..].copy_from_slice(&value.into_slice());
+                B256::from_slice(&bytes)
             }
         }
+    }
+}
+
+impl TryFrom<B256> for ProtocolVersion {
+    type Error = ProtocolVersionError;
+
+    fn try_from(value: B256) -> Result<Self, Self::Error> {
+        // <protocol version> ::= <version-type><typed-payload>
+        // <version-type> ::= <uint8>
+        // <typed-payload> ::= <31 bytes>
+        let version_type = value[0];
+        let typed_payload = &value[1..];
+
+        match version_type {
+            0 => Ok(Self::V0(ProtocolVersionFormatV0::try_from_slice(typed_payload)?)),
+            other => Err(ProtocolVersionError::UnsupportedVersion(other)),
+        }
+    }
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for ProtocolVersion {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        B256::from(*self).serialize(serializer)
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for ProtocolVersion {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let value = B256::deserialize(deserializer)?;
+        Self::try_from(value).map_err(serde::de::Error::custom)
     }
 }
 
@@ -98,42 +116,58 @@ pub struct ProtocolVersionFormatV0 {
     pub pre_release: u32,
 }
 
-impl From<ProtocolVersionFormatV0> for [u8; 31] {
-    fn from(value: ProtocolVersionFormatV0) -> Self {
-        // Version-type 0:
-        //
-        // <reserved><build><major><minor><patch><pre-release>
-        // <reserved> ::= <7 zeroed bytes>
-        // <build> ::= <8 bytes>
-        // <major> ::= <big-endian uint32>
-        // <minor> ::= <big-endian uint32>
-        // <patch> ::= <big-endian uint32>
-        // <pre-release> ::= <big-endian uint32>
-
-        let mut bytes = [0u8; 31];
-        bytes[0..7].copy_from_slice(&[0u8; 7]);
-        bytes[7..15].copy_from_slice(&value.build.0);
-        bytes[15..19].copy_from_slice(&value.major.to_be_bytes());
-        bytes[19..23].copy_from_slice(&value.minor.to_be_bytes());
-        bytes[23..27].copy_from_slice(&value.patch.to_be_bytes());
-        bytes[27..31].copy_from_slice(&value.pre_release.to_be_bytes());
-        bytes
+impl std::fmt::Display for ProtocolVersionFormatV0 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{build}.{major}.{minor}.{patch}-{pre_release}",
+            build = self.build,
+            major = self.major,
+            minor = self.minor,
+            patch = self.patch,
+            pre_release = self.pre_release
+        )
     }
 }
 
-impl TryFrom<&[u8]> for ProtocolVersionFormatV0 {
-    type Error = TryFromSliceError;
+impl ProtocolVersionFormatV0 {
+    /// Version-type 0 byte encoding:
+    ///
+    /// ```text
+    /// <reserved><build><major><minor><patch><pre-release>
+    /// <reserved> ::= <7 zeroed bytes>
+    /// <build> ::= <8 bytes>
+    /// <major> ::= <big-endian uint32>
+    /// <minor> ::= <big-endian uint32>
+    /// <patch> ::= <big-endian uint32>
+    /// <pre-release> ::= <big-endian uint32>
+    /// ```
+    pub fn into_slice(self) -> [u8; 31] {
+        let mut bytes = [0u8; 31];
+        bytes[0..7].copy_from_slice(&[0u8; 7]);
+        bytes[7..15].copy_from_slice(&self.build.0);
+        bytes[15..19].copy_from_slice(&self.major.to_be_bytes());
+        bytes[19..23].copy_from_slice(&self.minor.to_be_bytes());
+        bytes[23..27].copy_from_slice(&self.patch.to_be_bytes());
+        bytes[27..31].copy_from_slice(&self.pre_release.to_be_bytes());
+        bytes
+    }
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        // Version-type 0:
-        //
-        // <reserved><build><major><minor><patch><pre-release>
-        // <reserved> ::= <7 zeroed bytes>
-        // <build> ::= <8 bytes>
-        // <major> ::= <big-endian uint32>
-        // <minor> ::= <big-endian uint32>
-        // <patch> ::= <big-endian uint32>
-        // <pre-release> ::= <big-endian uint32>
+    /// Version-type 0 byte encoding:
+    ///
+    /// ```text
+    /// <reserved><build><major><minor><patch><pre-release>
+    /// <reserved> ::= <7 zeroed bytes>
+    /// <build> ::= <8 bytes>
+    /// <major> ::= <big-endian uint32>
+    /// <minor> ::= <big-endian uint32>
+    /// <patch> ::= <big-endian uint32>
+    /// <pre-release> ::= <big-endian uint32>
+    /// ```
+    fn try_from_slice(value: &[u8]) -> Result<Self, ProtocolVersionError> {
+        if value.len() != 31 {
+            return Err(ProtocolVersionError::InvalidLength(value.len()));
+        }
 
         Ok(Self {
             build: B64::from_slice(&value[7..15]),
