8261b0633e7763fa72b73d7cc48772eb9f84dae4
refcell <abigger87@gmail.com>
2024-09-25 10:31:47 -0400

    feat(genesis): optimism base fee params

--- crates/genesis/src/params.rs
+++ crates/genesis/src/params.rs
@@ -31,59 +31,69 @@ pub const OP_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER: u128 = 6;
 pub const BASE_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER: u128 = 10;
 
 /// Get the base fee parameters for Optimism Sepolia.
-pub const OP_SEPOLIA_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
-    max_change_denominator: OP_SEPOLIA_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR,
-    elasticity_multiplier: OP_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
-};
-
-/// Get the base fee parameters for Optimism Sepolia (post Canyon).
-pub const OP_SEPOLIA_CANYON_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
-    max_change_denominator: OP_SEPOLIA_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
-    elasticity_multiplier: OP_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
+pub const OP_SEPOLIA_BASE_FEE_PARAMS: OptimismBaseFeeParams = OptimismBaseFeeParams {
+    base_fee_params: BaseFeeParams {
+        max_change_denominator: OP_SEPOLIA_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR,
+        elasticity_multiplier: OP_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
+    },
+    eip1559_denominator_canyon: OP_SEPOLIA_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
 };
 
 /// Get the base fee parameters for Base Sepolia.
-pub const BASE_SEPOLIA_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
-    max_change_denominator: OP_SEPOLIA_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR,
-    elasticity_multiplier: BASE_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
-};
-
-/// Get the base fee parameters for Base Sepolia (post Canyon).
-pub const BASE_SEPOLIA_CANYON_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
-    max_change_denominator: OP_SEPOLIA_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
-    elasticity_multiplier: BASE_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
+pub const BASE_SEPOLIA_BASE_FEE_PARAMS: OptimismBaseFeeParams = OptimismBaseFeeParams {
+    base_fee_params: BaseFeeParams {
+        max_change_denominator: OP_SEPOLIA_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR,
+        elasticity_multiplier: BASE_SEPOLIA_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
+    },
+    eip1559_denominator_canyon: OP_SEPOLIA_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
 };
 
 /// Get the base fee parameters for Optimism Mainnet.
-pub const OP_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
-    max_change_denominator: OP_MAINNET_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR,
-    elasticity_multiplier: OP_MAINNET_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
-};
-
-/// Get the base fee parameters for Optimism Mainnet (post Canyon).
-pub const OP_CANYON_BASE_FEE_PARAMS: BaseFeeParams = BaseFeeParams {
-    max_change_denominator: OP_MAINNET_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
-    elasticity_multiplier: OP_MAINNET_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
+pub const OP_MAINNET_BASE_FEE_PARAMS: OptimismBaseFeeParams = OptimismBaseFeeParams {
+    base_fee_params: BaseFeeParams {
+        max_change_denominator: OP_MAINNET_EIP1559_DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR,
+        elasticity_multiplier: OP_MAINNET_EIP1559_DEFAULT_ELASTICITY_MULTIPLIER,
+    },
+    eip1559_denominator_canyon: OP_MAINNET_EIP1559_BASE_FEE_MAX_CHANGE_DENOMINATOR_CANYON,
 };
 
 /// Returns the [BaseFeeParams] for the given chain id.
-pub const fn base_fee_params(chain_id: u64) -> BaseFeeParams {
+pub const fn base_fee_params(chain_id: u64) -> OptimismBaseFeeParams {
     match chain_id {
-        10 => OP_BASE_FEE_PARAMS,
+        10 => OP_MAINNET_BASE_FEE_PARAMS,
         11155420 => OP_SEPOLIA_BASE_FEE_PARAMS,
-        8453 => OP_BASE_FEE_PARAMS,
+        8453 => OP_MAINNET_BASE_FEE_PARAMS,
         84532 => BASE_SEPOLIA_BASE_FEE_PARAMS,
-        _ => OP_BASE_FEE_PARAMS,
+        _ => OP_MAINNET_BASE_FEE_PARAMS,
     }
 }
 
-/// Returns the Canyon [BaseFeeParams] for the given chain id.
-pub const fn canyon_base_fee_params(chain_id: u64) -> BaseFeeParams {
-    match chain_id {
-        10 => OP_CANYON_BASE_FEE_PARAMS,
-        11155420 => OP_SEPOLIA_CANYON_BASE_FEE_PARAMS,
-        8453 => OP_CANYON_BASE_FEE_PARAMS,
-        84532 => BASE_SEPOLIA_CANYON_BASE_FEE_PARAMS,
-        _ => OP_CANYON_BASE_FEE_PARAMS,
+/// Optimism Base Fee Configuration
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct OptimismBaseFeeParams {
+    /// Base fee params
+    #[cfg_attr(feature = "serde", serde(flatten))]
+    pub base_fee_params: BaseFeeParams,
+    /// EIP 1559 Denominator for the Canyon hardfork
+    #[cfg_attr(
+        feature = "serde",
+        serde(rename = "eip1559DenominatorCanyon", alias = "eip1559_denominator_canyon")
+    )]
+    pub eip1559_denominator_canyon: u128,
+}
+
+impl OptimismBaseFeeParams {
+    /// Returns the inner [BaseFeeParams].
+    pub const fn as_base_fee_params(&self) -> BaseFeeParams {
+        self.base_fee_params
+    }
+
+    /// Returns the [BaseFeeParams] for the canyon hardfork.
+    pub const fn as_canyon_base_fee_params(&self) -> BaseFeeParams {
+        BaseFeeParams {
+            max_change_denominator: self.eip1559_denominator_canyon,
+            elasticity_multiplier: self.base_fee_params.elasticity_multiplier,
+        }
     }
 }
