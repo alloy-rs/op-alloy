c2861d6af228e9853c5ee8bc66e971c001852fb0
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-03 13:58:04 +0100

    chore: rustmft

--- crates/rpc-jsonrpsee/src/traits.rs
+++ crates/rpc-jsonrpsee/src/traits.rs
@@ -136,14 +136,12 @@ pub trait EngineApiExt {
     async fn signal_superchain_v1(&self, signal: SuperchainSignal) -> RpcResult<ProtocolVersion>;
 }
 
-
 /// Op API extension for controlling the miner.
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "miner"))]
 #[cfg_attr(feature = "client", rpc(server, client, namespace = "miner"))]
 pub trait MinerApiExt {
-
-    /// Sets the maximum data availability size of any tx allowed in a block, and the total max l1 data size of the block. 0 means no maximum.
+    /// Sets the maximum data availability size of any tx allowed in a block, and the total max l1
+    /// data size of the block. 0 means no maximum.
     #[method(name = "setMaxDASize")]
     async fn set_max_da_size(&self, max_tx_size: U64, max_block_size: U64) -> RpcResult<()>;
-
 }
