30e6f030c48d7dc23c71516f7800ce979d5a220b
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-01-14 12:17:30 +0100

    Remove re-export of maili traits

--- crates/consensus/src/lib.rs
+++ crates/consensus/src/lib.rs
@@ -9,8 +9,6 @@
 
 extern crate alloc;
 
-pub use maili_common::{DepositTransaction, DepositTxEnvelope};
-
 mod receipt;
 pub use receipt::{OpDepositReceipt, OpDepositReceiptWithBloom, OpReceiptEnvelope, OpTxReceipt};
 
