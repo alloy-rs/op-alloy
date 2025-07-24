98e0c575b0fbee2d68823297d67abb92168e0d08
William Law <williamlaw.wtl@gmail.com>
2025-07-24 17:22:38 -0400

    feat-based naming + nits

--- crates/consensus/src/eip1559.rs
+++ crates/consensus/src/eip1559.rs
@@ -66,10 +66,13 @@ pub fn encode_holocene_extra_data(
     Ok(Bytes::copy_from_slice(&extra_data))
 }
 
-/// Decodes the Jovian 1599 parameters from the `extradata` bytes.
+/// Decodes the EIP-1559 parameters from `extra_data`,
+/// as well as the minimum base fee log2.
 ///
 /// Returns (`elasticity`, `denominator`, `min_base_fee_log2`)
-pub fn decode_jovian_extra_data(extra_data: &[u8]) -> Result<(u32, u32, u8), EIP1559ParamError> {
+pub fn decode_min_base_fee_extra_data(
+    extra_data: &[u8],
+) -> Result<(u32, u32, u8), EIP1559ParamError> {
     if extra_data.len() < 10 {
         return Err(EIP1559ParamError::NoEIP1559Params);
     }
@@ -86,13 +89,15 @@ pub fn decode_jovian_extra_data(extra_data: &[u8]) -> Result<(u32, u32, u8), EIP
     Ok((u32::from_be_bytes(elasticity), u32::from_be_bytes(denominator), min_base_fee_log2))
 }
 
-/// Encodes the Jovian 1599 parameters for the payload
-pub fn encode_jovian_extra_data(
+/// Encodes the EIP-1559 parameters for the payload,
+/// as well as the minimum base fee log2.
+pub fn encode_min_base_fee_extra_data(
     eip_1559_params: B64,
     default_base_fee_params: BaseFeeParams,
     min_base_fee_log2: u8,
 ) -> Result<Bytes, EIP1559ParamError> {
-    // 9 bytes: 1 byte for version (1) and 9 bytes for eip1559 params
+    // 10 bytes: 1 byte for version (1), 8 bytes for eip1559 params, and 1 byte for the minimum base
+    // fee log2
     let mut extra_data = [0u8; 10];
     extra_data[0] = 1;
     encode_eip_1559_params(eip_1559_params, default_base_fee_params, &mut extra_data)?;
@@ -139,7 +144,8 @@ mod tests {
     #[test]
     fn test_get_extra_data_jovian() {
         let eip_1559_params = B64::from_str("0x0000000800000008").unwrap();
-        let extra_data = encode_jovian_extra_data(eip_1559_params, BaseFeeParams::new(80, 60), 20);
+        let extra_data =
+            encode_min_base_fee_extra_data(eip_1559_params, BaseFeeParams::new(80, 60), 20);
         // check the version byte is 1 and the min_base_fee_log2 is 20
         assert_eq!(extra_data.unwrap(), Bytes::copy_from_slice(&[1, 0, 0, 0, 8, 0, 0, 0, 8, 20]));
     }
@@ -147,7 +153,8 @@ mod tests {
     #[test]
     fn test_get_extra_data_jovian_default() {
         let eip_1559_params = B64::ZERO;
-        let extra_data = encode_jovian_extra_data(eip_1559_params, BaseFeeParams::new(80, 60), 0);
+        let extra_data =
+            encode_min_base_fee_extra_data(eip_1559_params, BaseFeeParams::new(80, 60), 0);
         // check the version byte is 1 and the min_base_fee_log2 is 0
         assert_eq!(extra_data.unwrap(), Bytes::copy_from_slice(&[1, 0, 0, 0, 80, 0, 0, 0, 60, 0]));
     }
