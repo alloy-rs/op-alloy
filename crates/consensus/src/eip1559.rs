bbcf1664addc48d621c7bc6d704dfebc958a6212
William Law <williamlaw.wtl@gmail.com>
2025-07-24 15:46:07 -0400

    refactor logic into encode_eip1559_params

--- crates/consensus/src/eip1559.rs
+++ crates/consensus/src/eip1559.rs
@@ -3,6 +3,31 @@
 use alloy_eips::eip1559::BaseFeeParams;
 use alloy_primitives::{B64, Bytes};
 
+/// Encodes the `eip1559` parameters for the payload.
+fn encode_eip_1559_params(
+    eip_1559_params: B64,
+    default_base_fee_params: BaseFeeParams,
+    extra_data: &mut [u8],
+) -> Result<(), EIP1559ParamError> {
+    if eip_1559_params.is_zero() {
+        let max_change_denominator: u32 = (default_base_fee_params.max_change_denominator)
+            .try_into()
+            .map_err(|_| EIP1559ParamError::DenominatorOverflow)?;
+
+        let elasticity_multiplier: u32 = (default_base_fee_params.elasticity_multiplier)
+            .try_into()
+            .map_err(|_| EIP1559ParamError::ElasticityOverflow)?;
+
+        extra_data[1..5].copy_from_slice(&max_change_denominator.to_be_bytes());
+        extra_data[5..9].copy_from_slice(&elasticity_multiplier.to_be_bytes());
+    } else {
+        let (elasticity, denominator) = decode_eip_1559_params(eip_1559_params);
+        extra_data[1..5].copy_from_slice(&denominator.to_be_bytes());
+        extra_data[5..9].copy_from_slice(&elasticity.to_be_bytes());
+    }
+    Ok(())
+}
+
 /// Extracts the Holocene 1599 parameters from the encoded form:
 /// <https://github.com/ethereum-optimism/specs/blob/main/specs/protocol/holocene/exec-engine.md#eip1559params-encoding>
 ///
@@ -37,27 +62,7 @@ pub fn encode_holocene_extra_data(
 ) -> Result<Bytes, EIP1559ParamError> {
     // 9 bytes: 1 byte for version (0) and 8 bytes for eip1559 params
     let mut extra_data = [0u8; 9];
-    // If eip 1559 params aren't set, use the canyon base fee param constants
-    // otherwise use them
-    if eip_1559_params.is_zero() {
-        // Try casting max_change_denominator to u32
-        let max_change_denominator: u32 = (default_base_fee_params.max_change_denominator)
-            .try_into()
-            .map_err(|_| EIP1559ParamError::DenominatorOverflow)?;
-
-        // Try casting elasticity_multiplier to u32
-        let elasticity_multiplier: u32 = (default_base_fee_params.elasticity_multiplier)
-            .try_into()
-            .map_err(|_| EIP1559ParamError::ElasticityOverflow)?;
-
-        // Copy the values safely
-        extra_data[1..5].copy_from_slice(&max_change_denominator.to_be_bytes());
-        extra_data[5..9].copy_from_slice(&elasticity_multiplier.to_be_bytes());
-    } else {
-        let (elasticity, denominator) = decode_eip_1559_params(eip_1559_params);
-        extra_data[1..5].copy_from_slice(&denominator.to_be_bytes());
-        extra_data[5..9].copy_from_slice(&elasticity.to_be_bytes());
-    }
+    encode_eip_1559_params(eip_1559_params, default_base_fee_params, &mut extra_data)?;
     Ok(Bytes::copy_from_slice(&extra_data))
 }
 
@@ -70,27 +75,7 @@ pub fn encode_jovian_extra_data(
     // 9 bytes: 1 byte for version (1) and 8 bytes for eip1559 params
     let mut extra_data = [0u8; 9];
     extra_data[0] = 1;
-    // If eip 1559 params aren't set, use the canyon base fee param constants
-    // otherwise use them
-    if eip_1559_params.is_zero() {
-        // Try casting max_change_denominator to u32
-        let max_change_denominator: u32 = (default_base_fee_params.max_change_denominator)
-            .try_into()
-            .map_err(|_| EIP1559ParamError::DenominatorOverflow)?;
-
-        // Try casting elasticity_multiplier to u32
-        let elasticity_multiplier: u32 = (default_base_fee_params.elasticity_multiplier)
-            .try_into()
-            .map_err(|_| EIP1559ParamError::ElasticityOverflow)?;
-
-        // Copy the values safely
-        extra_data[1..5].copy_from_slice(&max_change_denominator.to_be_bytes());
-        extra_data[5..9].copy_from_slice(&elasticity_multiplier.to_be_bytes());
-    } else {
-        let (elasticity, denominator) = decode_eip_1559_params(eip_1559_params);
-        extra_data[1..5].copy_from_slice(&denominator.to_be_bytes());
-        extra_data[5..9].copy_from_slice(&elasticity.to_be_bytes());
-    }
+    encode_eip_1559_params(eip_1559_params, default_base_fee_params, &mut extra_data)?;
     Ok(Bytes::copy_from_slice(&extra_data))
 }
 
@@ -134,6 +119,7 @@ mod tests {
     fn test_get_extra_data_jovian() {
         let eip_1559_params = B64::from_str("0x0000000800000008").unwrap();
         let extra_data = encode_jovian_extra_data(eip_1559_params, BaseFeeParams::new(80, 60));
+        // specifically check the version byte is 1
         assert_eq!(extra_data.unwrap(), Bytes::copy_from_slice(&[1, 0, 0, 0, 8, 0, 0, 0, 8]));
     }
 
@@ -141,6 +127,7 @@ mod tests {
     fn test_get_extra_data_jovian_default() {
         let eip_1559_params = B64::ZERO;
         let extra_data = encode_jovian_extra_data(eip_1559_params, BaseFeeParams::new(80, 60));
+        // specifically check the version byte is 1
         assert_eq!(extra_data.unwrap(), Bytes::copy_from_slice(&[1, 0, 0, 0, 80, 0, 0, 0, 60]));
     }
 }
