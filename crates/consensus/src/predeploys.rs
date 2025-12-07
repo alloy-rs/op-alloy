afce3997fb0a18dbfe8e438e8ae799bb7d305b49
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-12-07 12:54:19 +0100

    Update predeploys.rs

--- crates/consensus/src/predeploys.rs
+++ crates/consensus/src/predeploys.rs
@@ -3,5 +3,5 @@
 use alloy_primitives::{address, Address};
 
 /// The address of the `L2ToL1MessagePasser` predeploy.
-pub const L2_TO_L1_MESSAGE_PASSER: Address =
+pub const L2_TO_L1_MESSAGE_PASSER_ADDRESS: Address =
     address!("0x4200000000000000000000000000000000000016");
