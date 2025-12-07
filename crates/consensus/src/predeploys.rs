f4eb8987a69364cd2854abd5852dfddaf62b8c80
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-12-07 12:48:57 +0100

    deleted comments

--- crates/consensus/src/predeploys.rs
+++ crates/consensus/src/predeploys.rs
@@ -1,15 +1,7 @@
 //! Addresses of OP Stack pre-deployed contracts.
-//!
-//! These are the addresses of contracts that are pre-deployed on every OP Stack chain.
-//! See <https://specs.optimism.io/protocol/predeploys.html> for more details.
 
 use alloy_primitives::{address, Address};
 
-/// The `L2ToL1MessagePasser` predeploy address.
-///
-/// This contract stores commitments to withdrawal transactions and is used for
-/// L2 to L1 message passing.
-///
-/// Address: `0x4200000000000000000000000000000000000016`
+/// The address of the `L2ToL1MessagePasser` predeploy.
 pub const L2_TO_L1_MESSAGE_PASSER: Address =
     address!("0x4200000000000000000000000000000000000016");
