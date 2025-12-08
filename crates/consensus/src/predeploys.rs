4aa7fce3be8920c8d9ab412e6d1a1288a7a8c30e
Tomass <155266802+zeroprooff@users.noreply.github.com>
2025-12-08 06:33:47 +0100

    fmt

--- crates/consensus/src/predeploys.rs
+++ crates/consensus/src/predeploys.rs
@@ -1,6 +1,6 @@
 //! Addresses of OP Stack pre-deployed contracts.
 
-use alloy_primitives::{address, Address};
+use alloy_primitives::{Address, address};
 
 /// The address of the `L2ToL1MessagePasser` predeploy.
 pub const L2_TO_L1_MESSAGE_PASSER_ADDRESS: Address =
